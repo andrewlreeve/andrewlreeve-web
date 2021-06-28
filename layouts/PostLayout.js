0b9502857f3f9f487653584062c32b89940da4f0
Andrew Reeve <andrew@andrewlreeve.com>
2021-06-28 19:35:13 +1200

    fix git url

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -6,7 +6,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 
-const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
+const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/main/data/blog/${fileName}`
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
     `${siteMetadata.siteUrl}/blog/${slug}`
